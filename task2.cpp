8b7c4dd16e7825d3e9ea814bc7b53f60bcc4077b
CodeKirov <93568292+TKirovAl@users.noreply.github.com>
2022-11-29 09:24:49 +0700

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -1,25 +1,40 @@
 #include <iostream>
-void inputCoordinates();
-int quater(int* x, int* y);
-void outputAnswer(int quater);
-int main() {
-    inputCoordinates();
-    return  0;
+
+using namespace std;
+
+void output(string otvet) 
+{
+    cout << otvet;
 }
-void inputCoordinates() {
-    int x, y;
-    std::cin >> x >> y;
-    outputAnswer(quater(&x, &y));
-}
-int quater(int* x, int* y) {
-    if (*x != 0 && *y != 0) {
-        if (*x > 0 && *y > 0) return 1;
-        if (*x < 0 && *y > 0) return 2;
-        if (*x < 0 && *y < 0) return 3;
-        else return 4;
+
+void solution(int x, int y) 
+{
+
+    string otvet;
+
+    if (x > 0 && y > 0) {
+        otvet = "I";
+    } if (x < 0 && y>0) {
+        otvet = "II";
+    } if (x < 0 && y < 0) {
+        otvet = "III";
+    } if (x > 0 && y < 0) {
+        otvet = "IV";
     }
-    else return 0;
+
+    output(otvet);
 }
-void outputAnswer(int quater) {
-    std::cout << quater;
+
+void input() 
+{
+    int x, y;
+    cin >> x >> y;
+
+    solution(x, y);
+}
+
+int main() 
+{
+    input();
+    return 0;
 }
